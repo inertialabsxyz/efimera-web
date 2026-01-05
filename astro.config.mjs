d5f60d982696db39517c80c28bc38816473d0b02
Andy Bell <andyjsbell@gmail.com>
2026-01-05 12:44:26 +0100

    fix: update to node 20

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  output: 'hybrid',
-  adapter: vercel(),
+  output: "hybrid",
+  adapter: vercel({
+    nodeVersion: "20.x",
+  }),
   image: {
-    domains: ['cdn.sanity.io'],
+    domains: ["cdn.sanity.io"],
   },
 });

e3e385a461aa2a7d93a6d7a5a0b5a33feca1fb4f
Andy Bell <andyjsbell@gmail.com>
2026-01-05 18:14:59 +0100

    fix: local server content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: "static",
+  output: "server",
   adapter: vercel({
     nodeVersion: "20.x",
   }),

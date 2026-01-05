84149a056eb7928cdd9dd58a6956ada91e590e71
Andy Bell <andyjsbell@gmail.com>
2026-01-05 17:34:50 +0100

    fix: magazine in spanish

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,18 +1,18 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
 
 export default defineConfig({
-  name: 'default',
-  title: 'Efimera Magazine',
+  name: "default",
+  title: "Efimera Revista",
 
-  projectId: 'msziqj4u',
-  dataset: 'efimera-data',
+  projectId: "msziqj4u",
+  dataset: "efimera-data",
 
   plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+});

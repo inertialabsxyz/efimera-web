88537449c77ff14d703d113bea2ed48c351d5c1c
Andy Bell <andyjsbell@gmail.com>
2026-01-05 12:53:50 +0100

    fix: hybrid version to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
   adapter: vercel({
     nodeVersion: "20.x",
   }),
